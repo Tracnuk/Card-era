f91f488d59eef2db247c848e18499aa1a98fe78c
Tracnuk <opauliivleva@gmail.com>
2026-01-26 19:16:53 +0300

    clear project

--- models/enemy.py
+++
@@ -1,7 +0,0 @@
-class Enemy:
-    def __init__(self, hp=40, energy=2, activ_cards=[], wasted_energy=0):
-        self.hp = hp
-        self.energy = energy
-        self.activ_card = activ_cards
-        self.wasted_energy = wasted_energy
-
--- services/game_service.py
+++
@@ -1,9 +0,0 @@
-
-
-class GameService:
-    def arena(self):
-        pass
-    def shop(self):
-        pass
-    def inventary(self):
-        pass
